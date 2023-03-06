7a18671ca8c77703a886b9524967fbcc3937e5b1
Zera <bczaicki@gmail.com>
2023-03-06 21:17:45 +0100

    First project

--- Lab01/main.py
+++ Lab01/main.py
@@ -1,16 +1,86 @@
-# This is a sample Python script.
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def adjacency_matrix_to_adjacency_list(matrix):
+    adjacency_list = []
+    for row in matrix:
+        adjacency_list.append([])
+        for i in range(len(row)):
+            if row[i] == 1:
+                adjacency_list[-1].append(i + 1)
+
+    return adjacency_list
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def adjacency_list_to_adjacency_matrix(adjacency_list):
+    adjacency_matrix = np.zeros((len(adjacency_list), len(adjacency_list)), dtype=int)
+    index = 0
+    for row in adjacency_list:
+        for column in row:
+            adjacency_matrix[index][column - 1] = 1
+        index += 1
+
+    return adjacency_matrix
 
 
-# Press the green button in the gutter to run the script.
+def adjacency_matrix_to_incidence_matrix(adjacency_matrix):
+    num_nodes = len(adjacency_matrix)
+    num_edges = sum(sum(row) for row in adjacency_matrix) // 2
+    incidence_matrix = [[0 for j in range(num_edges)] for i in range(num_nodes)]
+    edge_index = 0
+    for i in range(num_nodes):
+        for j in range(i+1, num_nodes):
+            if adjacency_matrix[i][j] == 1:
+                incidence_matrix[i][edge_index] = 1
+                incidence_matrix[j][edge_index] = 1
+                edge_index += 1
+    return incidence_matrix
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    path = "Lab01/data/" + input("Please provide path to file\n")
+    type = int(input("Please provide type number according to the type of data in file\n1 - Adjacency matrix\n2 - Adjacency list\n3 - Incident matrix\n"))
+
+    adjacency_matrix = []
+    adjacency_list = []
+    incident_matrix = []
+
+    # if provided file contains adjacent matrix
+    if type == 1:
+        adjacency_matrix = np.loadtxt(path).astype(int)
+        adjacency_list = adjacency_matrix_to_adjacency_list(adjacency_matrix)
+        incident_matrix = adjacency_matrix_to_incidence_matrix(adjacency_matrix)
+
+    # if provided file contains adjacency list
+    elif type == 2:
+        with open(path, 'r') as f:
+            data = f.readlines()
+
+        adjacency_list = []
+        for line in data[:12]:
+            adjacency_list.append(list(map(int, line.strip().split())))
+
+        adjacency_matrix = adjacency_list_to_adjacency_matrix(adjacency_list)
+        incident_matrix = adjacency_matrix_to_incidence_matrix(adjacency_matrix)
+
+    # if provided file contains incident Matrix
+    elif type == 3:
+        pass
+    else :
+        print("Unknown number provided :(")
+
+    print("Adjacency matrix:")
+    for row in adjacency_matrix:
+        print(row)
+    print("Adjacency list:")
+    for row in adjacency_list:
+        print(row)
+    print("Incidence matrix:")
+    for row in incident_matrix:
+        print(row)
+
+    G = nx.from_numpy_array(adjacency_matrix)
+    nx.draw_circular(G, with_labels=True)
+    plt.axis('equal')
+    plt.show()
---
+++ Lab01/zad3.py
@@ -0,0 +1,40 @@
+import random
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+def generateRandomGraphNodesLines(nodes, lines):
+    incident_matrix = [[0 for i in range(lines)] for j in range(nodes)]
+
+    for column in range(lines):
+        selectedNodes = random.choices(range(nodes), k=2)
+        incident_matrix[selectedNodes[0]][column] = 1
+        incident_matrix[selectedNodes[1]][column] = 1
+
+
+    # G = nx.from_numpy_array(np.array())
+    # nx.draw_circular(G, with_labels=True)
+    # plt.axis('equal')
+    # plt.show()
+
+    return incident_matrix
+
+
+def generateRandomGraphNodesProbability(nodes, probability):
+    adjacency_matrix = [[0 for i in range(nodes)] for j in range(nodes)]
+    for row in range(nodes):
+        for column in range(row):
+            if random.choices([0, 1], weights=[1-probability, probability])[0] == 1:
+                adjacency_matrix[row][column] = 1
+                adjacency_matrix[column][row] = 1
+
+    G = nx.from_numpy_array(np.array(adjacency_matrix))
+    nx.draw_circular(G, with_labels=True)
+    plt.axis('equal')
+    plt.show()
+
+    return adjacency_matrix
+
+
+
+generateRandomGraphNodesProbability(15, 0.3)
